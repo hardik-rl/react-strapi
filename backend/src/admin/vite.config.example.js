6eeff7ae15824666bd829c887e9bde252b0a9598
hardik-rl <hardik.radicalloop@gmail.com>
2025-12-31 22:11:39 +0530

    backend config changes

--- backend/src/admin/vite.config.example.js
+++ backend/src/admin/vite.config.example.js
@@ -1,11 +1,28 @@
-const { mergeConfig } = require('vite');
+// const { mergeConfig } = require('vite');
+
+// module.exports = (config) => {
+//   // Important: always return the modified config
+//   return mergeConfig(config, {
+//     resolve: {
+//       alias: {
+//         '@': '/src',
+//       },
+//     },
+//   });
+// };
+
+const { mergeConfig } = require("vite");
 
 module.exports = (config) => {
-  // Important: always return the modified config
   return mergeConfig(config, {
+    server: {
+      allowedHosts: [
+        "react-strapi-ztyl.onrender.com",
+      ],
+    },
     resolve: {
       alias: {
-        '@': '/src',
+        "@": "/src",
       },
     },
   });
