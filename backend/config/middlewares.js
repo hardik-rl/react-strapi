dcf5948019f84e621bd091d49b7c9e9989571f18
hardik-rl <hardik.radicalloop@gmail.com>
2026-01-01 21:40:09 +0530

    middleware changes

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,9 +1,34 @@
-module.exports = [
-  'strapi::logger',
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+export default [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        // 'https://your-frontend-domain.onrender.com',
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
